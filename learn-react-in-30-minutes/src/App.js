2eb01f17232ea2fb1a22169e37f1f97f03f0aaf2
Garima <paudelgarima229@gmail.com>
2023-02-16 21:57:00 +0545

    first commit

---
+++ learn-react-in-30-minutes/src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import TodoList from "./TodoList";
+
+function App() {
+  return (
+    <>
+     <TodoList />
+     
+     <input type="text" />
+     <button>Add ToDo</button>
+     <button>Clear Complete</button>
+
+     </>
+
+     );
+  
+   
+  
+}
+
+export default App;
---
+++ learn-react-in-30-minutes/src/TodoList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+export default function TodoList(){
+return(
+<div>
+   <h1> Hello there!</h1>
+</div>
+)
+       
+        
+} 
+ 
